c536816d65f46373747a130c60e2f47a8b84641e
dtarasov <dtarasov7@gmail.com>
2021-02-27 15:51:09 +0300

    common

---
+++ far2l/ol8.3/build_far2l_ol83.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+sudo dnf -y install epel-release
+sudo dnf -y install gawk m4 glib2-devel wxGTK3-devel cmake gcc-c++ git make
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf config-manager --set-enabled ol8_codeready_builder
+
+sudo dnf -y install libssh-devel
+sudo dnf -y install libsmbclient-devel
+sudo dnf -y install libnfs-devel
+sudo dnf -y install libarchive-devel
+sudo dnf -y install python3-devel
+sudo dnf -y install virtualenv
+sudo dnf -y install openssl-devel
+sudo dnf -y install neon-devel
+sudo dnf -y install fmt-devel
+sudo dnf -y install xerces-c-devel
+sudo dnf -y install uchardet-devel
+sudo dnf -y install spdlog-devel
+
+rm -rf ./far2l
+git clone https://github.com/elfmz/far2l
+cd ./far2l
+mkdir ./build
+cd ./build
+cmake3 -DUSEWX=no -DPYTHON=yes -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+sudo make install
+cd ../..
+rm -rf ./far2l
