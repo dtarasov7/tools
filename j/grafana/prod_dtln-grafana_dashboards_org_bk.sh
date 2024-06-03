103a2c347cb8be3c027bfe0274831f2ee722fc3e
dtarasov7 <dtarasov7@gmail.com>
2024-06-03 08:26:03 +0300

    gg4

--- j/grafana/prod_dtln-grafana_dashboards_org_bk.sh
+++ j/grafana/prod_dtln-grafana_dashboards_org_bk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-HOST='http://10.80.252.20:3000'
+HOST='http://10.:3000'
 DASH_DIR=/data/minio/grafana/prod_dtln
 
 # Declare a list with the api keys using as a prefix the organization name plus "_" character
-declare -a StringArray=("main_eyJrIjoiN1QzVkFTOVpxcnByS3BpcWZJWGViT3JKMU9nNlJDR08iLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjF9" "admin-dtln_eyJrIjoiOTBUTExDYXJSZzZqOEY5TXhyZHdYMndUY091azF2SlQiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjJ9" "svoe-dtln_eyJrIjoiRmpxaEs2dEozSkR6a3VnMDNobWE0VUZTZzBqMG5tNFUiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjR9" "cos-dtln_eyJrIjoiR1pGWGxxUERXN2VXVGZHT1hCZVdrb1dTc0R1WlI4RXYiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjN9" "digital-back_eyJrIjoiV0dPclV5QlNrb1QzdUxHTGhEaEc3aDNhaENpT1liSWUiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjV9" "ndbo-dtln_eyJrIjoiQjVnQTdUN3BSUjcwaDJ5MnZBMEJXaEFVS1FESzVlRlciLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjZ9" "dit-dtln_eyJrIjoiVUZxeE96ZnJWNklpT2RkOGsxbWV3WWUzZnRJdG5KN1UiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjh9" "lt-dtln_eyJrIjoiS2hPY2tpUVFjc3hNMEFlQWhWQU5WOHkzRlZFc1RUcGEiLCJuIjoiZXhwb3J0X2ltcG9ydF9kYXNoYm9hcmRzIiwiaWQiOjd9")
+declare -a StringArray=("main_eyJrF9" "adm" "svozIiwiaWQiOjR9" "cosQiOjN9" "digWQiOjV9" "ndbIiwiaWQiOjZ9" "dit-d9ydF9kYXNo" "lt-dtln_eyJrIjoi")
 
 # Iterate through api keys:
 for API_KEY in "${StringArray[@]}"; do
